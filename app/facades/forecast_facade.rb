d10372bb3b2e7bc89c2adc65ecc01a9a20985295
Casey Fazio <98674727+casefaz@users.noreply.github.com>
2022-08-06 14:40:20 -0600

    Add forecast test and functionality for facade

--- app/facades/forecast_facade.rb
+++ app/facades/forecast_facade.rb
@@ -1,3 +1,6 @@
 class ForecastFacade
-  def
+  def self.forecast(lat, lon)
+    weather = WeatherService.get_weather(lat, lon)
+    Forecast.new(weather)
+  end
 end
--- spec/facades/forecast_facade_spec.rb
+++ spec/facades/forecast_facade_spec.rb
@@ -4,12 +4,13 @@ RSpec.describe ForecastFacade do
   it 'returns forecast objects' do
     coordinates = File.read("spec/fixtures/lat_lng.json")
     response = JSON.parse(coordinates, symbolize_names: true)
-    binding.pry
+
     lat = response[:results].first[:locations].first[:latLng][:lat]
     lon = response[:results].first[:locations].first[:latLng][:lng]
 
-    weather = WeatherService.get_weather(lat, lon)
-    binding.pry
-    Forecast.new(weather)
+
+    forecast = ForecastFacade.forecast(lat, lon)
+
+    expect(forecast).to be_an_instance_of(Forecast)
   end
 end
