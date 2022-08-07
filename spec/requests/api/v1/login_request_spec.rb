ec7baf8cafb49eb2b7cfefddf384688ed2cd8b91
Casey Fazio <98674727+casefaz@users.noreply.github.com>
2022-08-07 13:58:31 -0600

    Add more robust happy path tests for login

--- spec/requests/api/v1/login_request_spec.rb
+++ spec/requests/api/v1/login_request_spec.rb
@@ -9,21 +9,32 @@ RSpec.describe 'Login Endpoint' do
         "password_confirmation"=>"ThisisReal123"
       }
   
-    headers = { 'CONTENT_TYPE' => 'application/json'}
+      headers = { 'CONTENT_TYPE' => 'application/json'}
 
-    post "/api/v1/users", headers: headers, params: JSON.generate(create_user)
+      post "/api/v1/users", headers: headers, params: JSON.generate(create_user)
 
-    find_user = {
-      "email"=>"doesntgetmore@fakity.com",
-      "password"=>"ThisisReal123"
-    }
-  
-    headers = { 'CONTENT_TYPE' => 'application/json'}
+      find_user = {
+        "email"=>"doesntgetmore@fakity.com",
+        "password"=>"ThisisReal123"
+      }
+    
+      headers = { 'CONTENT_TYPE' => 'application/json'}
 
-    post "/api/v1/sessions", headers: headers, params: JSON.generate(find_user)
+      post "/api/v1/sessions", headers: headers, params: JSON.generate(find_user)
 
-    expect(response).to be_successful
-    expect(response).to have_http_status(200)
+      expect(response).to be_successful
+      expect(response).to have_http_status(200)
+      parsed_body = JSON.parse(response.body, symbolize_names: true)
+
+      # binding.pry
+      expect(parsed_body).to have_key(:data)
+      expect(parsed_body[:data].count).to eq(3)
+      expect(parsed_body[:data]).to have_key(:type)
+      expect(parsed_body[:data][:type]).to eq("users")
+      expect(parsed_body[:data][:attributes].count).to eq(2)
+      expect(parsed_body[:data][:attributes]).to have_key(:email)
+      expect(parsed_body[:data][:attributes]).to have_key(:api_key)
+      expect(parsed_body[:data][:attributes]).to_not have_key(:password)
     end
   end
 end
