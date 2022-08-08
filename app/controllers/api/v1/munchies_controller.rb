95b637956d96e29cd024fae06db2e2177edce5e9
Casey Fazio <98674727+casefaz@users.noreply.github.com>
2022-08-08 11:01:23 -0600

    Beginning sad path testing

--- app/controllers/api/v1/munchies_controller.rb
+++ app/controllers/api/v1/munchies_controller.rb
@@ -1,9 +1,13 @@
 class Api::V1::MunchiesController < ApplicationController
   def index
     # binding.pry
-    city = MapquestFacade.get_coordinates(params[:location])
-    forecast = ForecastFacade.forecast(city.lat, city.lon)
-    restaurant =  BusinessFacade.find_restaurants(params[:location], params[:food])
-    render json: MunchiesSerializer.locate_restaurants(params[:location], restaurant, forecast)
+    if params[:location] != nil && params[:food] != nil
+      city = MapquestFacade.get_coordinates(params[:location])
+      forecast = ForecastFacade.forecast(city.lat, city.lon)
+      restaurant =  BusinessFacade.find_restaurants(params[:location], params[:food])
+      render json: MunchiesSerializer.locate_restaurants(params[:location], restaurant, forecast)
+    else
+      render json: {error: user.errors.full_messages.to_sentence}, status: 400
+    end 
   end
 end
--- spec/requests/api/v1/munchies_request_spec.rb
+++ spec/requests/api/v1/munchies_request_spec.rb
@@ -1,26 +1,42 @@
 require 'rails_helper'
 
-RSpec.describe 'Muchies API' do 
-  it 'returns the requested information' do 
-    city = 'denver,co'
-    type = 'chinese'
-    weather = File.read('spec/fixtures/weather.json')
+RSpec.describe 'Muchies API' do
+  context 'happy path' do 
+    it 'returns the requested information' do 
+      city = 'denver,co'
+      type = 'chinese'
+      # weather = File.read('spec/fixtures/weather.json')
 
-    get "/api/v1/munchies?location=#{city}&food=#{type}"
-    # binding.pry
-    expect(response).to be_successful
-    expect(response).to have_http_status(200)
+      get "/api/v1/munchies?location=#{city}&food=#{type}"
+      # binding.pry
+      expect(response).to be_successful
+      expect(response).to have_http_status(200)
 
-    yum = JSON.parse(response.body, symbolize_names: true)
-    # binding.pry
-    expect(yum).to be_a(Hash)
-    expect(yum).to have_key(:data)
-    expect(yum[:data].count).to eq(3)
-    expect(yum[:data][:attributes].count).to eq(3)
-    expect(yum[:data][:attributes]).to have_key(:destination_city)
-    expect(yum[:data][:attributes][:forecast]).to have_key(:summary)
-    expect(yum[:data][:attributes][:forecast]).to have_key(:temperature)
-    expect(yum[:data][:attributes][:restaurant]).to have_key(:name)
-    expect(yum[:data][:attributes][:restaurant]).to have_key(:address)
+      yum = JSON.parse(response.body, symbolize_names: true)
+      # binding.pry
+      expect(yum).to be_a(Hash)
+      expect(yum).to have_key(:data)
+      expect(yum[:data].count).to eq(3)
+      expect(yum[:data][:type]).to eq("munchie")
+      expect(yum[:data][:attributes].count).to eq(3)
+      expect(yum[:data][:attributes]).to have_key(:destination_city)
+      expect(yum[:data][:attributes][:forecast]).to have_key(:summary)
+      expect(yum[:data][:attributes][:forecast]).to have_key(:temperature)
+      expect(yum[:data][:attributes][:restaurant]).to have_key(:name)
+      expect(yum[:data][:attributes][:restaurant]).to have_key(:address)
+    end
+  end
+
+  context 'sad path' do 
+    it 'returns an error if fields are missing' do 
+      city = ''
+      type = 'chinese'
+      # weather = File.read('spec/fixtures/weather.json')
+
+      get "/api/v1/munchies?location=#{city}&food=#{type}"
+
+      expect(response).to_not be_successful
+      expect(response).to have_http_status(400)
+    end
   end
 end
