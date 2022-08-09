39695dca4c3109e5b204b1d238221e06ff2b006e
Casey Fazio <98674727+casefaz@users.noreply.github.com>
2022-08-09 14:11:14 -0600

    Adjust serializer for conditional of impossible route

--- app/serializers/trip_weather_serializer.rb
+++ app/serializers/trip_weather_serializer.rb
@@ -7,7 +7,7 @@ class TripWeatherSerializer
         "attributes": {
           "start_city": eta.travel_data.origin.titleize,
           "end_city": eta.travel_data.destination.titleize,
-          "travel_time": eta.travel_data.travel_time,
+          "travel_time": eta.route_possibilities,
           "weather_at_eta": if eta.weather_at_eta.nil?
                               { }
                             else 
