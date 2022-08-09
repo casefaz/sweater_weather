ec981fb409041aa7f6a80d3f8f5dc4eeacda51db
Casey Fazio <98674727+casefaz@users.noreply.github.com>
2022-08-09 11:15:28 -0600

    Update Roadtrip Facade

--- spec/facades/road_trip_facade_spec.rb
+++ spec/facades/road_trip_facade_spec.rb
@@ -2,12 +2,14 @@ require 'rails_helper'
 
 RSpec.describe RoadTripFacade do 
   it 'returns the length of the roadtrip', :vcr do 
-    origin = 'denver,co'
-    destination = 'chicago,il'
+    origin = 'Denver'
+    destination = 'Chicago'
 
     length = RoadTripFacade.get_length(origin, destination)
 
     expect(length).to be_an_instance_of(RoadTrip)
     expect(length.travel_time).to eq("14:24:27")
+    expect(length.origin).to eq('Denver')
+    expect(length.destination).to eq('Chicago')
   end
 end
